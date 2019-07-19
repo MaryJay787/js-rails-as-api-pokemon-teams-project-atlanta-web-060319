bf0b1b3c693279b201e42cea397a952b9c3690f4
MaryJay787 <josephmaryam.mj@gmail.com>
2019-07-19 16:37:50 -0400

    still working

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,63 @@
+document.addEventListener('DOMContentLoaded', setUpPage)
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+function setUpPage(){
+    getAllTrainers()
+}
+
+function getAllTrainers(){
+    fetch(TRAINERS_URL)
+    .then(res => res.json())
+    .then(trainers => listAllTrainers(trainers))
+}
+
+function listAllTrainers(trainers){
+    trainers.forEach(train => trainerCard(train))
+    
+}
+
+
+function trainerCard(train){
+    let main = document.querySelector('.main')
+    let div = document.createElement('div')
+    div.className = 'card'
+    div.dataset
+    
+    let p = document.createElement('p')
+    p.innerText = train.name
+
+    let addPokemon = document.createElement('button')
+    addPokemon.innerText = 'Add Pokemon'
+    addPokemon.dataset.id = train.id
+    
+    let ul = document.createElement('ul')
+    train.pokemons.forEach(function(poke){
+        let li = document.createElement('li')
+        li.innerText = poke.nickname
+        li.setAttribute('class', 'class')
+        
+        let releaseBtn = document.createElement('button')
+        releaseBtn.innerText = 'Release'
+        releaseBtn.addEventListener('click', handleRelease)
+        releaseBtn.setAttribute('class', 'release')
+        releaseBtn.dataset.id = poke.trainer_id
+        
+        li.appendChild(releaseBtn)
+        ul.appendChild(li)
+        
+    })
+
+    function handleRelease(e){
+        console.log(e.target.querySelector('class'))
+    }
+    
+
+    main.appendChild(div)
+    div.appendChild(p)
+    div.appendChild(addPokemon)
+
+    div.appendChild(ul)
+}
+
